7d127f41ae175af41e20edc83dce2154d9c26e0c
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-03-25 16:46:44 -0300

    Primer commit terminado

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,43 @@
+import './mylabel.css';
+import { AllCaps } from '../stories/components/MyLabel.stories';
+
+export interface MyLabelProps {
+    /**
+     * Este es el texto que se mostrará en el label
+     */
+    label: string;
+    /**
+     * Este es el tamaño por defecto del label
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Defina si desea aplicar Capitalizacion en su Label
+     */
+    allCaps: boolean;
+    /**
+     * Defina que color desea aplicar a su Label
+     */
+    color: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Defina que color desea aplicar a su Label
+     */
+    fontColor?: string
+}
+
+
+export const MyLabel = ( { 
+    label   = 'No Label',
+    size    = 'normal',
+    allCaps = false,
+    color   = 'primary',
+    fontColor,
+}: MyLabelProps ) => {
+
+    return (
+
+        <span className={ `label ${ size } text-${ color } ` }  style={{ color: `${ fontColor }` }}>
+            { allCaps && label.toUpperCase() || label }
+        </span>
+
+    )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,54 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { MyLabel } from "../../components/MyLabel";
+
+
+export default { 
+    title:'UI/MyLabel',
+    component: MyLabel,
+    argTypes: {
+        color: { control: 'select', options: ['primary', 'secondary', 'tertiary'] },
+        fontColor: { control: 'color' },
+    }
+} as ComponentMeta< typeof MyLabel >
+
+const Template: ComponentStory< typeof MyLabel > = ( args ) => <MyLabel { ...args }/>
+
+export const Basic      = Template.bind({});
+Basic.args = {
+    label: 'No Label',
+    size: 'normal',
+    color: 'primary',
+    allCaps: false,
+}
+
+export const AllCaps    = Template.bind({});
+AllCaps.args = {
+    label: 'No Label',
+    size: 'normal',
+    color: 'primary',
+    allCaps: true,
+}
+
+export const Secondary  = Template.bind({});
+Secondary.args = { 
+    label: 'No Label',
+    size: 'normal',
+    color: 'secondary',
+    allCaps: false,
+}
+
+export const Tertiary  = Template.bind({});
+Tertiary.args = { 
+    label: 'No Label',
+    size: 'normal',
+    color: 'tertiary',
+    allCaps: true,
+}
+
+export const customColor  = Template.bind({});
+Tertiary.args = { 
+    label: 'No Label',
+    size: 'h1',
+    color: 'primary',
+    fontColor: '#000000',
+}
